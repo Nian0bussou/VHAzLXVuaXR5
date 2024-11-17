0d7d780fc0b787379b21a99a701b975176dfa60f
Nian0bussou <nianlegare@outlook.com>
2024-11-17 14:03:39 -0500

    started a bit position graph builder.cs

--- Assets/Code/PositionGraphBuilder.cs
+++ Assets/Code/PositionGraphBuilder.cs
@@ -3,73 +3,73 @@ using GraphLib;
 using System;
 using UnityEditor;
 
-public class PositionGraphBuilder : MonoBehaviour
-{
-    public enum Representation { Matrix, List };
+public class PositionGraphBuilder : MonoBehaviour {
+        public enum Representation { Matrix, List };
 
-    [SerializeField]
-    private Representation representation;
+        [SerializeField]
+        private Representation representation;
 
-    [SerializeField]
-    private GridAsset grid;
+        [SerializeField]
+        private GridAsset grid;
 
-    public Vector3[] positions;
-    public IGraph Graph { get; private set; }
+        public Vector3[] positions;
+        public IGraph Graph { get; private set; }
 
-    private void Awake() {
-        //COMPLÉTEZ À PARTIR D'ICI
+        private void Awake() {
+                //COMPLï¿½TEZ ï¿½ PARTIR D'ICI
 
-        //1. Créez la bonne sorte d'IGraph selon la valeur de `representation`
-        
-        //2. Ajoutez les liens ("edges") au IGraph
+                //1. Crï¿½ez la bonne sorte d'IGraph selon la valeur de `representation`
 
-    }
+                //2. Ajoutez les liens ("edges") au IGraph
 
-    private void OnDrawGizmos()
-    {
-        if (!Application.isPlaying) return;
-
-
-        Gizmos.color = Color.green; //set la couleur pour les sphères et lignes à vert
-        GUIStyle textStyle = new GUIStyle(); //créer un style pour le texte
-        textStyle.normal.textColor = Color.black; //écrire le texte en noir
-
-        //COMPLÉTEZ À PARTIR D'ICI
-
-        //1. Affichez une sphère pour chaque noeud à sa position associée
-        //   - Utilisez la méthode `Gizmos.DrawSphere(.)`
-        //   - Utilisez un rayon de 0.2f
-        
-        //2. Affichez une ligne entre les noeuds connectés par un lien
-        //   - Utilisez la méthode `Gizmos.DrawLine(.)`
-        
-        //3. Affichez le coût de chaque lien au centre de leur ligne
-        //   - Utilisez `Vector3.Lerp(a, b, t)`
-        //   - Utilisez `Handles.Label(position, texte, textStyle)
-
-        //4. Affichez une petite sphère verte pour représenter une pointe de flèche
-        //   - Utilisez `Gizmos.DrawSphere(.)
-        //   - Utilisez un rayon de 0.1f
-       
-    }
-
-    //OnValidate pourrait sans doute être optimisée un peu, mais bon...
-    private void OnValidate()
-    {
-        if (grid == null || grid.Size == 0)
-        {
-            positions = new Vector3[0];
-            return;
         }
 
-        int previousN = positions.Length;
-        var previousPositions = new Vector3[previousN];
+        private void OnDrawGizmos() {
+                if (!Application.isPlaying) return;
 
-        Array.Copy(positions, previousPositions, previousN);
 
-        positions = new Vector3[grid.Size];
+                Gizmos.color = Color.green; //set la couleur pour les sphï¿½res et lignes ï¿½ vert
+                GUIStyle textStyle = new(); //crï¿½er un style pour le texte
+                textStyle.normal.textColor = Color.black; //ï¿½crire le texte en noir
 
-        int min = Mathf.Min(grid.Size, previousN);
-        Array.Copy(previousPositions, positions, min);
-    }
+                //COMPLï¿½TEZ ï¿½ PARTIR D'ICI
+
+                //1. Affichez une sphï¿½re pour chaque noeud ï¿½ sa position associï¿½e
+                //   - Utilisez la mï¿½thode `Gizmos.DrawSphere(.)`
+                //   - Utilisez un rayon de 0.2f
+                Gizmos.DrawSphere(new(0, 0, 0), 0.2f);
+
+
+                //2. Affichez une ligne entre les noeuds connectï¿½s par un lien
+                //   - Utilisez la mï¿½thode `Gizmos.DrawLine(.)`
+
+                Gizmos.DrawLine(from, to); // TODO
+
+                //3. Affichez le coï¿½t de chaque lien au centre de leur ligne
+                //   - Utilisez `Vector3.Lerp(a, b, t)`
+                //   - Utilisez `Handles.Label(position, texte, textStyle)
+
+                //4. Affichez une petite sphï¿½re verte pour reprï¿½senter une pointe de flï¿½che
+                //   - Utilisez `Gizmos.DrawSphere(.)
+                //   - Utilisez un rayon de 0.1f
+
+        }
+
+        //OnValidate pourrait sans doute ï¿½tre optimisï¿½e un peu, mais bon...
+        private void OnValidate() {
+                if (grid == null || grid.Size == 0) {
+                        positions = new Vector3[0];
+                        return;
+                }
+
+                int previousN = positions.Length;
+                var previousPositions = new Vector3[previousN];
+
+                Array.Copy(positions, previousPositions, previousN);
+
+                positions = new Vector3[grid.Size];
+
+                int min = Mathf.Min(grid.Size, previousN);
+                Array.Copy(previousPositions, positions, min);
+        }
 }
